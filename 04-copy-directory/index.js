4ea5d607055a8a0517fb2c4174109c908c8c16e8
Antonina Berchuk <tonyaberch@gmail.com>
2021-11-02 21:30:00 +0100

    fix: change copyDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,18 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 function copyDir() {
-  fs.promises.rmdir(path.resolve(__dirname, 'files-copy'), { 'recursive': true })
-    .then(() =>
-      fs.promises.mkdir(path.resolve(__dirname, 'files-copy'), { 'recursive': true }))
-    .then(() => {
-      fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
-        files.forEach(file => {
-          fs.copyFile(path.resolve(__dirname, 'files', file), path.resolve(__dirname, 'files-copy', file), (err) => {
-            if (err) throw err;
-          });
-        });
+  fs.promises.mkdir(path.resolve(__dirname, 'files-copy'), { 'recursive': true });
+  fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
+    files.forEach(file => {
+      fs.copyFile(path.resolve(__dirname, 'files', file), path.resolve(__dirname, 'files-copy', file), (err) => {
+        if (err) throw err;
       });
     });
+  });
 }
 
 copyDir();
