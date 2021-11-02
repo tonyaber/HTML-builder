4af5b1dda39e4faae6ad7d68b1253d5b9915c027
Antonina Berchuk <tonyaberch@gmail.com>
2021-11-02 18:41:56 +0100

    feat: create 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.resolve(__dirname, 'secret-folder'), (err, files) => {
+  if (!err) {
+    files.forEach(file => {
+      fs.promises.stat(path.resolve(__dirname, 'secret-folder', file))
+        .then(statFile => {
+          if (!statFile.isDirectory()) {
+            console.log(`${file.substring(0, file.lastIndexOf('.'))} - ${path.extname(file.toString()).substring(1)} - ${statFile.size}b`)
+          }
+        });
+    });
+  }
+});
