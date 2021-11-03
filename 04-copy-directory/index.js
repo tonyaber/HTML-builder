7276d6f10a5f0e6ed0b14ac894e5608e9639a4aa
Antonina Berchuk <tonyaberch@gmail.com>
2021-11-03 17:41:59 +0100

    fix: add async and await in 04-copy-dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-function copyDir() {
-  fs.promises.mkdir(path.resolve(__dirname, 'files-copy'), { 'recursive': true });
-  fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
+async function copyDir() {
+  await fs.promises.rm(path.resolve(__dirname, 'files-copy'), { recursive: true, force: true });
+  await fs.promises.mkdir(path.resolve(__dirname, 'files-copy'), { 'recursive': true });
+  await fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
     files.forEach(file => {
       fs.copyFile(path.resolve(__dirname, 'files', file), path.resolve(__dirname, 'files-copy', file), (err) => {
         if (err) throw err;
